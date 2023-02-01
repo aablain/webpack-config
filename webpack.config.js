8bbbf18715364cd2e1e9171713062dc4bd6db2ae
Austin Blain <austin@mocaworks.com>
2023-02-01 15:34:52 -0800

    Project: - Add support for compiling SCSS -> CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -6,23 +8,36 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   mode,
+
   devtool: "source-map",
+
   devServer: {
     static: "./dist",
+    hot: true,
   },
+
   module: {
     rules: [
       {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
         test: /\.js$/,
-      },
-      {
         exclude: /node_modules/,
-      },
-      {
         use: {
           loader: "babel-loader",
         },
       },
     ],
   },
+
+  target: ["web"],
+
+  plugins: [new MiniCssExtractPlugin()],
 };
